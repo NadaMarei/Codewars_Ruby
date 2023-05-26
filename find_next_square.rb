4fe33df842a6b6c6e262df70885d5e9cfe69831a
Nada Marei <118731723+NadaMarei@users.noreply.github.com>
2023-05-26 14:25:36 +0300

    Update find_next_square.rb

--- find_next_square.rb
+++ find_next_square.rb
@@ -1,4 +1,4 @@
-""You might know some pretty large perfect squares. But what about the NEXT one?
+"""You might know some pretty large perfect squares. But what about the NEXT one?
 
 Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
 
@@ -8,7 +8,7 @@ Examples:(Input --> Output)
 
 121 --> 144
 625 --> 676
-114 --> -1 since 114 is not a perfect square""
+114 --> -1 since 114 is not a perfect square"""
   
 #my answer:
   def find_next_square(sq)
